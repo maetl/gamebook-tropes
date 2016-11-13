1f8d41c6d2aaa8552ea4fe63093a90393a5ccc61
Mark Rickerby <me@maetl.net>
2016-11-13 20:14:27 +1100

    Nicer graphviz style

--- story.rb
+++ story.rb
@@ -11,9 +11,9 @@ viz[:bgcolor] = 'transparent'
 graph.edges.each do |edge|
   from = edge.from
   to = edge.to
-  viz.add_node(from.id.to_s, label: from.label, fontname: 'Lucida Grande', fontsize: '12', shape: 'rectangle', style: 'rounded', color: '.7 .3 1.0')
-  viz.add_node(to.id.to_s, label: to.label, fontname: 'Lucida Grande', fontsize: '12', shape: 'rectangle', style: 'rounded', color: '.7 .3 1.0')
+  viz.add_node(from.id.to_s, label: from.label, fontname: 'Lucida Grande', fontsize: '12', shape: 'rectangle', style: 'rounded,filled', fillcolor: '.7 .3 1.0')
+  viz.add_node(to.id.to_s, label: to.label, fontname: 'Lucida Grande', fontsize: '12', shape: 'rectangle', style: 'rounded,filled', fillcolor: '.7 .3 1.0')
   viz.add_edge(from.id.to_s, to.id.to_s)
 end
 
-viz.output(png: 'out/recursive-branches.png')
+viz.output(png: 'out/recursive-branches2.png')
